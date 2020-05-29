0c34162f1e9108fe2d6d90179f2208bec34d7355
Shivangi <sviradiya@njcu.edu>
2020-05-29 22:49:44 +0000

    This is my java program

---
+++ an.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class an{
+	 public static void main(String[] args)
+	 {
+
+
+		 System.out.println("Pass a value to stdin...");
+		 // Scanner scanner = new Scanner(System.in);
+		 // String input = scanner.next();
+		 // scanner.close();
+
+		 int num1 = 20,num2 = 20, sum;
+		 sum = num1 + num2;
+            
+		 System.out.println("Sum of these numbers = " +sum);
+
+
+		 System.err.println("This is an error message");
+
+	 
+	}
+}
